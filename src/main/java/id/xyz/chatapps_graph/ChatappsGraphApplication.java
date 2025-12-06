a2b4dbf91a0e4fc04e6784beba0d829818aa9a9f
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2025-12-06 19:04:40 +0700

    initial commit

---
+++ src/main/java/id/xyz/chatapps_graph/ChatappsGraphApplication.java
@@ -0,0 +1,13 @@
+package id.xyz.chatapps_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappsGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappsGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/xyz/chatapps_graph/ChatappsGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package id.xyz.chatapps_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappsGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
