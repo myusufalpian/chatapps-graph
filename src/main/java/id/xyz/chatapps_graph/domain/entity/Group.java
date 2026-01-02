a0c49d8e07fbfb2de46731626abe4a8c85a89364
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2026-01-02 20:00:27 +0700

    add new entity and adjust the db migration

---
+++ src/main/java/id/xyz/chatapps_graph/domain/entity/Group.java
@@ -0,0 +1,34 @@
+package id.xyz.chatapps_graph.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "\"group\"")
+@Getter
+@Setter
+public class Group extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "group_id")
+  private Long groupId;
+
+  @Column(name = "group_uuid", length = 64, columnDefinition = "varchar(64) default gen_random_uuid()")
+  private String groupUuid;
+
+  @Column(name = "group_name", length = 255)
+  private String groupName;
+
+  @Column(name = "group_desc", columnDefinition = "TEXT")
+  private String groupDesc;
+
+  @Column(name = "is_active", columnDefinition = "integer default 0")
+  private Integer isActive;
+}
--- src/main/java/id/xyz/chatapps_graph/domain/entity/User.java
+++ src/main/java/id/xyz/chatapps_graph/domain/entity/User.java
@@ -1,15 +1,46 @@
 package id.xyz.chatapps_graph.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+@Table(name = "Users")
+@Getter
+@Setter
+public class User extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long userId;
 
-    
+  @Column(name = "user_uuid", length = 64, columnDefinition = "varchar(64) default gen_random_uuid()")
+  private String userUuid;
+
+  @Column(name = "user_phone", length = 20)
+  private String userPhone;
+
+  @Column(name = "user_mail", length = 100)
+  private String userMail;
+
+  @Column(name = "user_full_name", length = 255)
+  private String userFullName;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "about_id")
+  private MstAbout about;
+
+  @Column(name = "is_backup_active", columnDefinition = "integer default 0")
+  private Integer isBackupActive;
+
+  @Column(name = "user_status", columnDefinition = "integer default 0")
+  private Integer userStatus;
 }
